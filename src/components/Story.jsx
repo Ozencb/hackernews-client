dd1b45e788517be4a1ea2a282395cbdb894f8e62
Özenç Bilgili <bilgiliozenc@gmail.com>
2020-06-20 19:25:46 +0300

    Add styled component wrappers

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,23 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { getStory } from '../services/api';
+import {
+  StoryWrapper, StoryTitle, StoryMeta, StoryMetaElement,
+} from '../styles/StoryStyles';
+import mapTime from '../mappers/mapTime';
 
 const Story = ({ storyId }) => {
   const [story, setStory] = useState({});
 
   useEffect(() => {
-    getStory(storyId).then((data) => { 
+    getStory(storyId).then((data) => {
       data && data.url && setStory(data);
     });
   }, []);
 
   return story && story.url ? (
-    <>
-      <a href={story.url}><p>{story.title}</p></a>
-      <p>By: {story.by}</p>
-      <p>Posted: {story.time}</p>
-      <hr/>
-      <br/>
-    </>
+    <StoryWrapper data-testid="story">
+      <StoryTitle>
+        <a href={story.url}>{story.title}</a>
+      </StoryTitle>
+
+      <StoryMeta data-testid="story-by">
+        <span>
+          <StoryMetaElement color="#000">By: </StoryMetaElement>
+          {story.by}
+          <StoryMetaElement margin="10px" color="#000">Time: </StoryMetaElement>
+          {mapTime(story.time)}
+        </span>
+      </StoryMeta>
+    </StoryWrapper>
   ) : null;
 };
 
--- src/containers/StoriesContainer.jsx
+++ src/containers/StoriesContainer.jsx
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { getStoryIds } from '../services/api';
 import Story from '../components/Story';
+import { GlobalStyle, StoriesContainerWrapper } from '../styles/StoriesContainerStyles';
+import useInfiniteScroll from '../hooks/useInfiniteScroll';
 
 const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
+  const { count } = useInfiniteScroll();
 
   useEffect(() => {
     getStoryIds().then((ids) => {
-      ids && setStoryIds(ids);      
+      ids && setStoryIds(ids);
     });
   }, []);
 
-  // storyIds.map((storyId) => <Story key={storyId} storyId={storyId} />)
+  return (
+    <>
+      <GlobalStyle />
+      <StoriesContainerWrapper>
+        <h1>Hackernews</h1>
 
-  return  <Story storyId={23583238} />
+        {storyIds.slice(0, count).map((storyId) => <Story key={storyId} storyId={storyId} />)}
+      </StoriesContainerWrapper>
+    </>
+  );
 };
 
 export default StoriesContainer;
