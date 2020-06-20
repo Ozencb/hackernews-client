0542a6ea8bf29d6d9f0677069a20d1e79ef9cd66
Özenç Bilgili <bilgiliozenc@gmail.com>
2020-06-20 19:22:12 +0300

    Filter API data

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { filterFields } from '../utils/filterFields'
+import { filterFields } from '../selectors/filterFields'
 
 export const baseUrl = 'https://hacker-news.firebaseio.com/v0';
 export const newStoriesUrl = `${baseUrl}/newstories.json`;
