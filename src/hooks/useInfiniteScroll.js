e2bfa589435ab2a11233f40ef96ffe5bf5379fec
Özenç Bilgili <bilgiliozenc@gmail.com>
2020-06-20 20:35:58 +0300

    Add scroll debouncing

--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -1,16 +1,20 @@
 import { useState, useEffect } from 'react';
 import { MAX_STORIES, STORY_INCREMENT } from '../constants/index';
+import debounce from '../utils/debounce';
 
 const useInfiniteScroll = () => {
   const [loading, setLoading] = useState(false);
   const [count, setCount] = useState(STORY_INCREMENT);
 
-  const handleScroll = () => {
-    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || loading) {
+  const handleScroll = debounce(() => {
+    if (window.innerHeight
+        + document.documentElement.scrollTop
+        !== document.documentElement.offsetHeight
+        || loading) {
       return false;
     }
     setLoading(true);
-  };
+  }, 100);
 
   useEffect(() => {
     if (!loading) return;
