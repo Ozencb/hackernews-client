6b95581cffe7fee20ecf9542baf046a573d109e1
Özenç Bilgili <bilgiliozenc@gmail.com>
2020-07-05 19:02:31 +0300

    Updates

---
+++ precache-manifest.bf860f85de3a15296097313446818d32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14aed31916356fa67585cb8215119641",
+    "url": "/hackernews-client/index.html"
+  },
+  {
+    "revision": "3621b3ff0d6ee2a4c9b7",
+    "url": "/hackernews-client/static/js/2.3188c354.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hackernews-client/static/js/2.3188c354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c9650830e7ee9827ab4",
+    "url": "/hackernews-client/static/js/main.4c4c68da.chunk.js"
+  },
+  {
+    "revision": "324114e893480735bc46",
+    "url": "/hackernews-client/static/js/runtime-main.e08f3c53.js"
+  }
+]);
